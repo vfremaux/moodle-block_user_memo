b51c604912f53f7ffd63b2943de68ad497f2d5f5
valery <valery.fremaux@gmail.com>
2016-11-29 10:50:11 +0100

    Automated sync from SVN

--- tests/generator/lib.php
+++ tests/generator/lib.php
@@ -25,7 +25,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-
 /**
  * Online users block data generator class
  *
@@ -44,8 +43,6 @@ class block_user_memo_generator extends testing_block_generator {
      */
     public function create_instance($record = null, array $options = null) {
         global $DB, $CFG;
-
-        require_once("$CFG->dirroot/mod/page/locallib.php");
 
         $this->instancecount++;
 
